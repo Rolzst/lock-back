d98b27ffce798777be81555ee5044e4e20458b84
jomarth <jomarthemanuel@gmail.com>
2024-12-12 16:00:03 -0600

    cors update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://lookbox.onrender.com/', 'http://localhost:5173', process.env.BASE_URL],
     credentials: true
 }));
 app.use(morgan('dev'));
